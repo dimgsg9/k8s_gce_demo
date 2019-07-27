dbe9bab5553e1e1356cb149e87d94728fa335f0c
Dima G <dimgsg9@localnet.local>
2019-07-27 23:54:12 +0800

    Parametrize GCP/GCE region for cluster deployment

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-MULTIZONE=true ENABLE_ETCD_QUORUM_READ=true KUBE_GCE_ZONE=asia-southeast1-a ./kubernetes/cluster/kube-up.sh
+GCE_REGION=$1
 
-#KUBE_GCE_ZONE=asia-southeast1-b KUBE_REPLICATE_EXISTING_MASTER=true ./kubernetes/cluster/kube-up.sh
+MULTIZONE=true ENABLE_ETCD_QUORUM_READ=true KUBE_GCE_ZONE="${GCE_REGION}-a" ./kubernetes/cluster/kube-up.sh
 
-#KUBE_GCE_ZONE=asia-southeast1-c KUBE_REPLICATE_EXISTING_MASTER=true ./kubernetes/cluster/kube-up.sh
+# Uncomment for HA deployment with multiple masters
+#KUBE_GCE_ZONE="${GCE_REGION}-b" KUBE_REPLICATE_EXISTING_MASTER=true ./kubernetes/cluster/kube-up.sh
+
+# Uncomment for HA deployment with multiple masters
+#KUBE_GCE_ZONE="${GCE_REGION}-c" KUBE_REPLICATE_EXISTING_MASTER=true ./kubernetes/cluster/kube-up.sh
